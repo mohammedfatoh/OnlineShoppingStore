dcc0825a7152e0277e28388c4a59631a2dfdf5c7
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-07-24 23:23:04 +0200

    user management 1 fatoh

--- OnlineShoppingStore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ OnlineShoppingStore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -65,13 +65,6 @@ namespace OnlineShoppingStore.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            [Required]
-            [Display(Name = "First Name")]
-            public string FirstName { get; set; }
-
-            [Required]
-            [Display(Name = "Last Name")]
-            public string LastName { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
