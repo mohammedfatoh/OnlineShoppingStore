0e56bf57e7593df3114835a8eb7c2b97bc05faaa
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-08-08 01:02:10 +0200

    showProductsOfCategory

--- OnlineShoppingStore/Controllers/CustomerController.cs
+++ OnlineShoppingStore/Controllers/CustomerController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShoppingStore.Models;
+using OnlineShoppingStore.Services;
 
 namespace OnlineShoppingStore.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly IServiceBase<Category> categoryService;
+        private readonly IServiceBase<Product> productService;
+
+
+        public CustomerController(IServiceBase<Category> categoryService, 
+            IServiceBase<Product> productService)
+            
+        {
+            this.productService = productService;
+            this.categoryService = categoryService;
+        }
         public IActionResult Index()
         {
+            List<string> Namescategories = new List<string>();
+            foreach (var category in categoryService.GetAll())
+            {
+                Namescategories.Add(category.Name);
+            }
+            ViewBag.NamesCategories = Namescategories;
             return View();
+        }
+
+        public IActionResult productsOfCategory(string categoryName)
+        {
+            List<string> Namescategories = new List<string>();
+            foreach (var category in categoryService.GetAll())
+            {
+                Namescategories.Add(category.Name);
+            }
+            ViewBag.NamesCategories = Namescategories;
+            return View((productService.GetAll().Where(p => p.Category.Name == categoryName)).ToList());
         }
     }
 }
--- OnlineShoppingStore/Controllers/HomeController.cs
+++ OnlineShoppingStore/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OnlineShoppingStore.Models;
+using OnlineShoppingStore.Services;
 using System.Diagnostics;
 
 namespace OnlineShoppingStore.Controllers
@@ -7,14 +8,24 @@ namespace OnlineShoppingStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IServiceBase<Category> categoryService;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger,
+            IServiceBase<Category> categoryService)
         {
             _logger = logger;
+            this.categoryService = categoryService;
         }
 
         public IActionResult Index()
         {
+            List<string> Namescategories = new List<string>();
+            foreach (var category in categoryService.GetAll())
+            {
+                Namescategories.Add(category.Name);
+            }
+            ViewBag.NamesCategories = Namescategories;
             return View();
         }
 
