1d530042bed47dd423d8a5af6643c62421a919ee
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-08-12 01:44:11 +0200

    editing Shopping cart

--- OnlineShoppingStore/Controllers/CustomerController.cs
+++ OnlineShoppingStore/Controllers/CustomerController.cs
@@ -66,7 +66,6 @@ namespace OnlineShoppingStore.Controllers
             {
                   var str = HttpContext.Session.GetString("cart");
                  var productSelected = JsonConvert.DeserializeObject<List<Product>>(str);
-                int g = productSelected.Count();
 
 
                 productSelected.Add(product);
@@ -100,6 +99,91 @@ namespace OnlineShoppingStore.Controllers
         public ActionResult CartShoppingEmpty()
         {
             return View();
+        }
+
+        public ActionResult RemoveProductfromCart(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            var productSeilalized = HttpContext.Session.GetString("cart");
+            var productSelected = JsonConvert.DeserializeObject<List<Product>>(productSeilalized);
+            
+             productSelected.Remove(productSelected.Find(p => p.Id == productId));
+           
+            
+                HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(productSelected, Formatting.None,
+                      new JsonSerializerSettings()
+                      {
+                          ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                      }));
+
+                ViewBag.cart = productSelected.Count();
+                int? count = HttpContext.Session.GetInt32("count");
+                HttpContext.Session.SetInt32("count", (int)(count - 1));
+
+               
+            
+            return View("Cart", productSelected);
+        }
+
+        public ActionResult SavaProductLaterShopping(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            if (String.IsNullOrEmpty(HttpContext.Session.GetString("Savedproducts")))
+            {
+                List<Product> productSelected = new List<Product>();
+
+                productSelected.Add(product);
+
+                HttpContext.Session.SetString("Savedproducts", JsonConvert.SerializeObject(productSelected, Formatting.None,
+                        new JsonSerializerSettings()
+                        {
+                            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                        }));
+                RemoveProductfromCart(productId);
+                ViewBag.cart = productSelected.Count();
+                HttpContext.Session.SetInt32("countSavedproducts", 1);
+                
+
+            }
+            else
+            {
+                var productsSerialized = HttpContext.Session.GetString("Savedproducts");
+                var productSelected = JsonConvert.DeserializeObject<List<Product>>(productsSerialized);
+
+
+                productSelected.Add(product);
+
+                HttpContext.Session.SetString("Savedproducts", JsonConvert.SerializeObject(productSelected, Formatting.None,
+                         new JsonSerializerSettings()
+                         {
+                             ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                         }));
+                HttpContext.Session.SetInt32("countSavedproducts", productSelected.Count());
+                RemoveProductfromCart(productId);
+
+            }
+            return RedirectToAction("Cart");
+        }
+        public ActionResult RemoveProductfromLaterList(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            var productSeilalized = HttpContext.Session.GetString("Savedproducts");
+            var Savedproducts = JsonConvert.DeserializeObject<List<Product>>(productSeilalized);
+
+            Savedproducts.Remove(Savedproducts.Find(p => p.Id == productId));
+
+
+            HttpContext.Session.SetString("Savedproducts", JsonConvert.SerializeObject(Savedproducts, Formatting.None,
+                  new JsonSerializerSettings()
+                  {
+                      ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                  }));
+
+            
+            int? count = HttpContext.Session.GetInt32("countSavedproducts");
+            HttpContext.Session.SetInt32("countSavedproducts", (int)(count - 1));
+
+            return RedirectToAction("Cart");
         }
 
         public ActionResult Checkout()
