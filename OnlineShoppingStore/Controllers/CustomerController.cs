3c31a1f94cac21050c0190e7f4e405adb82618f8
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-08-12 14:54:43 +0200

    finish cart

--- OnlineShoppingStore/Controllers/CustomerController.cs
+++ OnlineShoppingStore/Controllers/CustomerController.cs
@@ -45,6 +45,7 @@ namespace OnlineShoppingStore.Controllers
         public ActionResult AddToCart(int productId)
         {
             Product product =productService.GetDetails(productId);
+            decimal newprice = product.Price;
             if (String.IsNullOrEmpty(HttpContext.Session.GetString("cart")))
             {
                 List<Product> productSelected = new List<Product>();
@@ -67,8 +68,16 @@ namespace OnlineShoppingStore.Controllers
                   var str = HttpContext.Session.GetString("cart");
                  var productSelected = JsonConvert.DeserializeObject<List<Product>>(str);
 
-
-                productSelected.Add(product);
+                var ProductExist = productSelected.Find(p => p.Id == productId);
+               if(ProductExist != null)
+               {
+                    ProductExist.Quantity += 1;
+                    ProductExist.Price += newprice;
+                }
+                else
+                {
+                    productSelected.Add(product);
+                }
 
                 HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(productSelected, Formatting.None,
                          new JsonSerializerSettings()
@@ -79,6 +88,7 @@ namespace OnlineShoppingStore.Controllers
                 ViewBag.cart = productSelected.Count();
                 int? count = HttpContext.Session.GetInt32("count");
                 HttpContext.Session.SetInt32("count", (int)(count + 1));
+               
             }
             return RedirectToAction("productsOfCategory",new { categoryName = product.Category.Name } );
 
@@ -118,11 +128,12 @@ namespace OnlineShoppingStore.Controllers
 
                 ViewBag.cart = productSelected.Count();
                 int? count = HttpContext.Session.GetInt32("count");
-                HttpContext.Session.SetInt32("count", (int)(count - 1));
+                 if(count > 0)
+                     HttpContext.Session.SetInt32("count", (int)(count - 1));
 
                
             
-            return View("Cart", productSelected);
+            return RedirectToAction("Cart");
         }
 
         public ActionResult SavaProductLaterShopping(int productId)
@@ -181,9 +192,92 @@ namespace OnlineShoppingStore.Controllers
 
             
             int? count = HttpContext.Session.GetInt32("countSavedproducts");
-            HttpContext.Session.SetInt32("countSavedproducts", (int)(count - 1));
+            if (count > 0)
+               HttpContext.Session.SetInt32("countSavedproducts", (int)(count - 1));
 
             return RedirectToAction("Cart");
+        }
+
+        public ActionResult AddToCartfromLaterList(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            var productSeilalized = HttpContext.Session.GetString("Savedproducts");
+            var Savedproducts = JsonConvert.DeserializeObject<List<Product>>(productSeilalized);
+
+            AddToCart(productId);
+
+            Savedproducts.Remove(Savedproducts.Find(p => p.Id == productId));
+
+            HttpContext.Session.SetString("Savedproducts", JsonConvert.SerializeObject(Savedproducts, Formatting.None,
+                  new JsonSerializerSettings()
+                  {
+                      ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                  }));
+
+
+            int? count = HttpContext.Session.GetInt32("countSavedproducts");
+            if (count > 0)
+                HttpContext.Session.SetInt32("countSavedproducts", (int)(count - 1));
+
+            return RedirectToAction("Cart");
+        }
+
+
+        public ActionResult IncreaseproductQuantity(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            decimal Productprice= product.Price;
+            var str = HttpContext.Session.GetString("cart");
+            var productSelected = JsonConvert.DeserializeObject<List<Product>>(str);
+
+            var ProductExist = productSelected.Find(p => p.Id == productId);
+            if (ProductExist != null)
+            {
+                ProductExist.Quantity += 1;
+                ProductExist.Price += Productprice;
+            }
+            else
+            {
+                productSelected.Add(product);
+            }
+
+            HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(productSelected, Formatting.None,
+                     new JsonSerializerSettings()
+                     {
+                         ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                     }));
+
+            ViewBag.cart = productSelected.Count();
+            int? count = HttpContext.Session.GetInt32("count");
+            HttpContext.Session.SetInt32("count", (int)(count + 1));
+            return View("Cart", productSelected);
+        }
+
+        public ActionResult DecreaseproductQuantity(int productId)
+        {
+            Product product = productService.GetDetails(productId);
+            decimal Productprice = product.Price;
+            var str = HttpContext.Session.GetString("cart");
+            var productSelected = JsonConvert.DeserializeObject<List<Product>>(str);
+
+            var ProductExist = productSelected.Find(p => p.Id == productId);
+            if (ProductExist != null)
+            {
+                ProductExist.Quantity -= 1;
+                ProductExist.Price -= Productprice;
+            }
+
+            HttpContext.Session.SetString("cart", JsonConvert.SerializeObject(productSelected, Formatting.None,
+                     new JsonSerializerSettings()
+                     {
+                         ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                     }));
+
+            ViewBag.cart = productSelected.Count();
+            int? count = HttpContext.Session.GetInt32("count");
+            if(count > 0)
+               HttpContext.Session.SetInt32("count", (int)(count - 1));
+            return View("Cart", productSelected);
         }
 
         public ActionResult Checkout()
